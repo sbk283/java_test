df749080660d115328aeda6f1ac54c411a0ad45e
wasabikun1704 <sbk283@naver.com>
2023-09-19 08:51:24 +0900

    무야호

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,19 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         int a;
-        a = 5;
+        a = -1;
         System.out.println(a);
-        System.out.println(a + 10);
-
-        int b = 10;
-
-        System.out.println("a : " + a);
-        System.out.println("b : " + b);
-
-        a = 10;
-        b = 5;
-
-        System.out.println("a : " + a);
-        System.out.println("b : " + b);
+        System.out.println("무야호");
     }
 }
