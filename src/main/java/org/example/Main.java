9d9b9e2033e6d7aa6bbebcde9f776ec0ec9fc377
wasabikun1704 <sbk283@naver.com>
2023-09-18 12:09:57 +0900

    자습중

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,10 @@
 package org.example;
 
+import static java.lang.System.*;
+
 public class Main {
     public static void main(String[] args) {
-        // System.out 여기서 . 은 of 를 의미 합니다.
-        System.out.println(100); // 여기서 println(100); 와 같은 형태는 타 동사를 의미합니다.
-        System.out.println(100); // 여기서 System.out 은 주어를 의미 합니다.
-        System.out.println(100); // 여기서 100은 목적어 또는 보어 입니다.
-        System.out.println(100); // 개발자가 "System.out" 라는 녀석에게 100 을 println 하라는 의미
-
-        // 프로그램의 시작점
-        System.out.println(1); // 실행순서 1 : 숫자는 " 로 감싸지 않아도 됩니다.
-        System.out.println("2");
-        System.out.println("3");
-        System.out.println("사");
-        // 프로그램의 끝점
+        System.out.println("안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요\n");
+        out.println();
     }
 }
