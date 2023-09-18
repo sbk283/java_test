b1024fedfd3fc826a9a4cb6e1bd09d974e0fa849
wasabikun1704 <sbk283@naver.com>
2023-09-18 13:28:00 +0900

    변수바꾸기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,20 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
-        System.out.println();
-        System.out.println();
+        int a;
+        a = 5;
+        System.out.println(a);
+        System.out.println(a + 10);
+
+        int b = 10;
+
+        System.out.println("a : " + a);
+        System.out.println("b : " + b);
+
+        a = 10;
+        b = 5;
+
+        System.out.println("a : " + a);
+        System.out.println("b : " + b);
     }
 }
