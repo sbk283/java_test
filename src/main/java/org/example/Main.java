b5cc5552fee1ddd8a9a1bfcd94bfc2511c82a44b
wasabikun1704 <sbk283@naver.com>
2023-09-18 13:01:07 +0900

    syso로 커맨드 변경

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,9 @@
 package org.example;
 
-import static java.lang.System.*;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요\n안녕하세요\n");
-        out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
     }
 }
---
+++ src/main/java/org/example/test.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class test {
+    void t1 () {
+        System.out.println();
+    }
+}
