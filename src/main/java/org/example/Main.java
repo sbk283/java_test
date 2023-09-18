1120ae699a12aca85a9d087926b196b34ba41234
wasabikun1704 <sbk283@naver.com>
2023-09-18 11:30:35 +0900

    진짜최종본

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,12 +2,16 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welcome!\n");
         for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-            System.out.println("i = " + i);
-            System.out.println("i = " + i);
-            System.out.println("i = " + i);
+            if (i<3) {
+                System.out.println(i +"교시입니다.");
+            } else if (i<5) {
+                System.out.println(i +"교시입니다. 절반 왔습니다.");
+            } else {
+                System.out.println(i +"교시입니다. 거의다 끝났습니다.");
+            }
+
         }
     }
 }
